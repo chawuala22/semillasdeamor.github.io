c1f31a54e7adb22faff4c3196ddd62451fb27a1f
chawuala22 <mrojasgarcia22@gmail.com>
2023-04-16 21:03:21 -0500

    ajustes trayectoria

--- src/app/modules/home/iglesia/trayectoria/trayectoria.component.ts
+++ src/app/modules/home/iglesia/trayectoria/trayectoria.component.ts
@@ -8,21 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class TrayectoriaComponent implements OnInit {
   info = [
     {
-      img: 'https://static.vecteezy.com/system/resources/previews/005/911/641/non_2x/business-man-icon-user-account-free-vector.jpg',
+      img: '../../../../../assets/images/jesus.png',
       tittle: '+1.300 Personas',
       text: 'Han aceptado a Jesús',
     },
     {
-      img: 'https://static.vecteezy.com/system/resources/previews/005/911/641/non_2x/business-man-icon-user-account-free-vector.jpg',
+      img: '../../../../../assets/images/bautizo.png',
       tittle: '+540 Personas',
       text: 'Se han bautizado',
     },
     {
-      img: 'https://static.vecteezy.com/system/resources/previews/005/911/641/non_2x/business-man-icon-user-account-free-vector.jpg',
+      img: '../../../../../assets/images/matrimonioo.png',
       tittle: '80',
       text: 'Matrimonios celebrados',
     },
-    { img: 'https://static.vecteezy.com/system/resources/previews/005/911/641/non_2x/business-man-icon-user-account-free-vector.jpg', tittle: '3', text: 'Encuentros ABBA' },
+    { img: '../../../../../assets/images/encuentro.png', tittle: '3', text: 'Encuentros ABBA' },
   ];
 
   constructor() {}
